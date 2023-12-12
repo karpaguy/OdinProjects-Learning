9f27168dc651748687db9da734f62f2b55484941
karpaguy <denuzzidouglas@gmail.com>
2023-12-12 09:49:18 -0300

    Now the game lasts five rounds and reset

--- JS Rock-Paper-Scissors/script.js
+++ JS Rock-Paper-Scissors/script.js
@@ -1,34 +1,60 @@
 const buttons = document.querySelectorAll(".btn");
 const resultDisplay = document.querySelector(".resultDisplay");
 
+const playerScoreDisplay = document.querySelector(".playerScoreDisplay");
+const machineScoreDisplay = document.querySelector(".machineScoreDisplay");
+
+let playerScore = 0;
+let machineScore = 0;
+
 function getComputerChoice() {
-    let options = ["rock", "paper", "scissors"];
+    let options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * 3)];
+}
+
+function resetScore() {
+    playerScore = 0;
+    machineScore = 0;
+}
+
+function checkWinner() {
+    if (playerScore == 5) {
+        resultDisplay.textContent = `You've win the game!`;
+        resetScore();
+    }
+    else if (machineScore == 5) {
+        resultDisplay.textContent = `You've lost the game!`;
+        resetScore();
+    }
 }
 
 function playRound() {
     machineValue = getComputerChoice();
 
-    if (this.value == "scissors" && machineValue == "paper") { 
+    if (this.value == "Scissors" && machineValue == "Paper") { 
         resultDisplay.textContent = `You win! ${this.value} beats ${machineValue}`;
-        return 1;
+        playerScore += 1;
     }
-    else if (this.value == "rock" && machineValue == "scissors") { 
+    else if (this.value == "Rock" && machineValue == "Scissors") { 
         resultDisplay.textContent = `You win! ${this.value} beats ${machineValue}`;
-        return 1;
+        playerScore += 1;
     }
-    else if (this.value == "paper" && machineValue == "rock") { 
+    else if (this.value == "Paper" && machineValue == "Rock") { 
         resultDisplay.textContent = `You win! ${this.value} beats ${machineValue}`;
-        return 1;
+        playerScore += 1;
     }
     else if (this.value == machineValue ) { 
         resultDisplay.textContent = `Its a draw!`;
-        return 2;
     }
     else { 
         resultDisplay.textContent = `You Lose! ${machineValue} beats ${this.value}`;
-        return 0;
+        machineScore += 1;
     };
+
+    playerScoreDisplay.textContent = `Player - ${playerScore}`;
+    machineScoreDisplay.textContent = `Machine - ${machineScore}`;
+
+    checkWinner();
 }
 
 buttons.forEach((button) => {
