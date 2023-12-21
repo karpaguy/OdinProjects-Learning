466d69157d294e600741a89a6e3ec5aa4d7c144d
karpaguy <denuzzidouglas@gmail.com>
2023-12-21 16:40:21 -0300

    Grid size input works, but css for size needs changes

--- JS Etch-a-Sketech/script.js
+++ JS Etch-a-Sketech/script.js
@@ -1,11 +1,11 @@
-const container = document.querySelector('.container');
-
 function changeColor() {
     // this.classList.toggle("gridUnitBlack");
     this.style.backgroundColor = 'black';
 }
 
 function createGrid(num) {
+    num = num * num;
+
     for (let i = 0; i < num; i++) {
         const div = document.createElement('div');
         div.classList.add("gridUnit");
@@ -17,4 +17,15 @@ function createGrid(num) {
     }
 }
 
-createGrid(256);
+const container = document.querySelector('.container');
+const gridChangeBtn = document.querySelector('.gridChangeBtn')
+
+gridChangeBtn.addEventListener('click', () => {
+    const userGridValue = prompt('How many squares do you want? (Max 100)');
+    while (container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+    createGrid(userGridValue);
+});
+
+createGrid(16);
