629a272d33da0796ca769b6ec989856a6ac086d9
karpaguy <denuzzidouglas@gmail.com>
2023-12-30 17:04:44 -0300

    gridItens now auto-adjust, but only heigth

--- JS Etch-a-Sketech/script.js
+++ JS Etch-a-Sketech/script.js
@@ -1,14 +1,17 @@
 function changeColor() {
-    // this.classList.toggle("gridUnitBlack");
-    this.style.backgroundColor = 'black';
+    this.classList.toggle("gridUnitBlack");
+    // this.style.backgroundColor = 'black';
 }
 
 function createGrid(num) {
+    let value = String(512 / num);
     num = num * num;
 
     for (let i = 0; i < num; i++) {
         const div = document.createElement('div');
         div.classList.add("gridUnit");
+
+        this.style.width = value;
 
         div.addEventListener("mouseover", changeColor);
 
@@ -28,4 +31,15 @@ gridChangeBtn.addEventListener('click', () => {
     createGrid(userGridValue);
 });
 
-createGrid(16);
+createGrid(4);
+
+/*
+Idea 1
+GridItem widht needs to be 512 divided by the number passed as an argument on createGrid.
+
+Idea 2
+Make it using div rows instead of placing each div individually.
+For example, if it is createGrid(16), it will create 16 containers, each with 16 divs.
+They will be placed one after another in the proprer way.
+So when the flex-grow happens, maybe it will work.  
+*/
