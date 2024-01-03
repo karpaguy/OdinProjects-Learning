296e8dba7f61a941d1c00c51c6013029ebbf4499
karpaguy <denuzzidouglas@gmail.com>
2024-01-03 10:39:42 -0300

    The penMode method works, but the option buttons are bugged

--- JS Etch-a-Sketech/script.js
+++ JS Etch-a-Sketech/script.js
@@ -1,30 +1,29 @@
-function changeColorToBlack() {
-    this.classList.toggle("gridUnitBlack");
+function changeColorToBlack(item) {
+    item.classList.toggle("gridUnitBlack");
     // this.style.backgroundColor = 'black';
 }
 
-function changeColorToRainbow() {
+function changeColorToRainbow(item) {
     let rainbowColors = ['#e81416', '#ffa500', '#faeb36', '#79c314', '#487de7', '#4b369d', '#70369d'];
 
     let randowColor = Math.floor(Math.random() * 7)
     console.log(randowColor);
-    this.style.backgroundColor = rainbowColors[randowColor]
+    item.style.backgroundColor = rainbowColors[randowColor]
 }
 
-// The penMode is a prototype to choose the color of background.
-// Kinda complicated, but it helps to add a new pen mode easliy.
+var penMode = 'black';
+function paintSquare(e) {
+    console.log(penMode);
 
-// var penMode = 'black';
-// function paintSquare(e) {
-//     console.log('WA');
-
-//     switch (penMode) {
-//         case 'black':
-//             changeColorToBlack();
-//         case 'rainbow':
-//             changeColorToRainbow();
-//     };
-// }
+    switch (penMode) {
+        case 'black':
+            changeColorToBlack(this);
+            break
+        case 'rainbow':
+            changeColorToRainbow(this);
+            break
+    };
+}
 
 function createGrid(num) {
     // 512 comes from the container size. Can be changed to a variable if desired.
@@ -39,15 +38,26 @@ function createGrid(num) {
 
         div.style.width = widhtPerSquare + "px";
 
-        div.addEventListener("mouseover", changeColorToRainbow);
+        div.addEventListener("mouseover", paintSquare);
 
         console.log("grid!");
         container.appendChild(div);
     }
 }
 
+// function changePen(color) {
+//     penMode = color;
+//     console.log(penMode)
+// }
+
 const container = document.querySelector('.gridContainer');
 const gridChangeBtn = document.querySelector('.gridChangeBtn');
+
+const blackPenBtn = document.querySelector('.blackPen');
+blackPenBtn.addEventListener('click', changePen('black'));
+
+const rainbowPenBtn = document.querySelector('.rainbowPen');
+rainbowPenBtn.addEventListener('click', changePen('rainbow'));
 
 gridChangeBtn.addEventListener('click', () => {
     const userGridValue = prompt('How many squares do you want? (Max 100)');
